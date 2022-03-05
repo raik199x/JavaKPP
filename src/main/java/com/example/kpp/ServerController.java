b6019d46f874a078c4ac14c9072ac01c633f3a3b
Alexander <70070040+raik199x@users.noreply.github.com>
2022-03-05 06:35:27 +0000

    Update ServerController.java

--- src/main/java/com/example/kpp/ServerController.java
+++ src/main/java/com/example/kpp/ServerController.java
@@ -7,11 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ServerController {
     @GetMapping(path = "/polynom")
-    public String WelcomMessage(@RequestParam(name="MyPath",defaultValue = "empty") String test){
+    public RawData WelcomMessage(@RequestParam(name="MyPath",defaultValue = "empty") String test){
 
         for(int i = 0; i < test.length(); i++)
-            if(test.charAt(i) != test.charAt(test.length()-i-1))
-                return "The word is: " + test + " - " + Integer.toString(test.length()) + " symbols" + "\nIt's not a polynom!";
-        return "The word is: " + test + " - " + Integer.toString(test.length()) + " symbols" + "\nIts a polynom!";
+            if(test.charAt(i) != test.charAt(test.length()-i-1)) {
+                return new RawData(test,test.length()-1,false);
+                //return "The word is: " + test + " - " + Integer.toString(test.length()) + " symbols" + "\nIt's not a polynom!";
+            }
+        return new RawData(test,test.length(),true);
+        //return "The word is: " + test + " - " + Integer.toString(test.length()) + " symbols" + "\nIts a polynom!";
     }
 }
