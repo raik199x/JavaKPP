254c51b373daf763e0aeb29259d7e36a30955cae
Alexander <70070040+raik199x@users.noreply.github.com>
2022-03-05 18:15:51 +0000

    Update ServerController.java

--- src/main/java/com/example/kpp/ServerController.java
+++ src/main/java/com/example/kpp/ServerController.java
@@ -1,20 +1,15 @@
 package com.example.kpp;
 
+import com.example.kpp.logic.polynom;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ServerController {
-    @GetMapping(path = "/polynom")
-    public RawData WelcomMessage(@RequestParam(name="MyPath",defaultValue = "empty") String test){
 
-        for(int i = 0; i < test.length(); i++)
-            if(test.charAt(i) != test.charAt(test.length()-i-1)) {
-                return new RawData(test,test.length(),false);
-                //return "The word is: " + test + " - " + Integer.toString(test.length()) + " symbols" + "\nIt's not a polynom!";
-            }
-        return new RawData(test,test.length(),true);
-        //return "The word is: " + test + " - " + Integer.toString(test.length()) + " symbols" + "\nIts a polynom!";
+    @GetMapping(path = "/polynom")
+    public String[][] WelcomMessage(@RequestParam(name="MyPath",defaultValue = "empty") String test){
+        return polynom.is_polynom(test);
     }
 }
