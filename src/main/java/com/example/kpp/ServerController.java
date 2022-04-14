6f18e5688ce906fe9e074098f4a8a76a2d3d73db
Alexander <csstraic@mail.ru>
2022-04-14 20:06:40 +0300

    Added test version of multithreading

--- src/main/java/com/example/kpp/ServerController.java
+++ src/main/java/com/example/kpp/ServerController.java
@@ -1,5 +1,6 @@
 package com.example.kpp;
 
+import com.example.kpp.ThreadLogic.MultiThread;
 import com.example.kpp.cache.GetCache;
 import com.example.kpp.exceptionhandling.MyThrowable;
 import com.example.kpp.logic.Polynom;
@@ -15,16 +16,24 @@ public class ServerController {
 
     static Logger MyLogger = LoggerFactory.getLogger(Polynom.class);
     private GetCache getCache;
+    private MultiThread MyThread;
 
     @Autowired
     public void SetCache(GetCache getCache){
         this.getCache = getCache;
     }
+    @Autowired
+    public void CreateThread(MultiThread NewThread){
+        this.MyThread = NewThread;
+    }
+
+
 
     @GetMapping(path = "/polynom")
     public String[][] WelcomMessage(@RequestParam(name="MyPath",defaultValue = "empty") String test) throws MyThrowable {
 
         String[][] reply = new String[3][2];
+        MyThread.IncCalls();
         //checking cache
         reply = getCache.Get(test);
         if(!reply[0][0].equals("FALSE")) {
@@ -34,5 +43,9 @@ public class ServerController {
         reply = Polynom.is_polynom(test);
         getCache.Set(test,reply);
         return reply;
+    }
+    @GetMapping(path = "/Calls")
+    public int ShowCalls(){
+        return MyThread.AskCalls();
     }
 }
