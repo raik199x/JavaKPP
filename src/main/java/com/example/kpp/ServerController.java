a1d049d36aebd473c8fe57ea8f9178c8cf660f06
Alexander <csstraic@mail.ru>
2022-04-29 22:25:51 +0300

    Post mapping created

--- src/main/java/com/example/kpp/ServerController.java
+++ src/main/java/com/example/kpp/ServerController.java
@@ -1,6 +1,10 @@
 package com.example.kpp;
 
 import com.example.kpp.ThreadLogic.MultiThread;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import com.example.kpp.ThreadLogic.RequestCounter;
 import com.example.kpp.cache.GetCache;
 import com.example.kpp.exceptionhandling.MyThrowable;
@@ -8,9 +12,9 @@ import com.example.kpp.logic.Polynom;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ServerController {
@@ -43,4 +47,27 @@ public class ServerController {
     public int ShowCalls(){
         return RequestCounter.AskCounter();
     }
+    @PostMapping(path = "/polynom")
+    public ResponseEntity<?> ManyData(@RequestBody ArrayList<String> data){
+        ArrayList<String> Results = new ArrayList<>();
+        MyLogger.info("Post request is in task");
+        AtomicInteger TruePolynoms = new AtomicInteger();
+        AtomicInteger FalsePolynoms = new AtomicInteger();
+        data.forEach((currentElement) -> {
+            try {
+                String result = Polynom.is_polynom(currentElement)[2][1];
+                Results.add(result);
+                if(result == "true")
+                    TruePolynoms.getAndIncrement();
+                else
+                    FalsePolynoms.getAndIncrement();
+            } catch (MyThrowable e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Results.add("True: " + TruePolynoms);
+        Results.add("False: "+ FalsePolynoms);
+        return new ResponseEntity<>(Results, HttpStatus.OK);
+    }
+
 }
